d1756d57fef6ffc6017719ce28fea296efafbeaa
ahmed <236643686+jacko111197-netizen@users.noreply.github.com>
2026-06-21 20:23:07 +0200

    railway URL

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@ window.CONFIG = {
   API_BASE_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:8000"
-      : "https://compiler-dap2-production.up.railway.app"
+      : "compiler-rm-dap2-production.up.railway.app"
 };
